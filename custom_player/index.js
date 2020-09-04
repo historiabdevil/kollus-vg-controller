425f94d7032dcd83bb608f71cee9f92db87ad0ca
HyeonD <hdayng@catenoid.net>
2020-09-04 11:26:00 +0900

    add custom_player

--- custom_player/index.js
+++ custom_player/index.js
@@ -26,6 +26,7 @@ window.onload = function () {
     singleTap.requireFailure([tripleTap, doubleTap]);
 
     touchManager.on('singletap', function (ev) {
+        ev.preventDefault();
         console.log('SingleTap')
         showControlbar();
         setTimeout(function () {
@@ -33,11 +34,11 @@ window.onload = function () {
         }, showTimeControlbar * 1000);
     });
     touchManager.on('doubletap', function (ev) {
+        ev.preventDefault();
         console.log('DoubleTap : ' + JSON.stringify(ev.center))
         if (touchpannel.clientWidth / 2 < ev.center.x) {
             controller.ff();
         } else {
-
             controller.rw();
         }
     });
